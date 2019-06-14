76b235591bd598692be4740632d1348b31726b70
ParkerCode <jjp316@ufl.edu>
2019-06-13 22:46:10 -0400

    using wave for audio

--- src/audio_processing.py
+++ src/audio_processing.py
@@ -2,21 +2,23 @@ from scipy import signal as sg
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
+import wave
 
 # Matplotlib signal test
-t = np.linspace(0, 1, 500, endpoint=False)
-plt.plot(t, np.sin(2 * np.pi * 5 * t))
-plt.ylim(-2, 2)
-plt.show()
-
+# t = np.linspace(0, 1, 500, endpoint=False)
+# plt.plot(t, np.sin(2 * np.pi * 5 * t))
+# plt.ylim(-2, 2)
+# plt.show()
 
 sampling_rate = 44100
-freq = 1500
-samples = 44100
-x = np.arange(samples)
+freq = 400
+channels = 1
+sample_width = 2
+x = np.arange(sampling_rate)
 
 # Sine
 y = 100 * np.sin(2 * np.pi * freq * x / sampling_rate)
+# y = 100 * sin(2pi * freq * x / sampling_rate
 
 # Square
 # y = 100 * sg.square(2 * np.pi * freq * x / sampling_rate)
@@ -27,11 +29,18 @@ y = 100 * np.sin(2 * np.pi * freq * x / sampling_rate)
 # Sawtooth
 # y = 100 * sg.sawtooth(2 * np.pi * freq * x / sampling_rate)
 
-f = open('test.wav', 'wb')
+wav = wave.open('test.wav', 'wb')
+wav.setnchannels(channels)
+wav.setsampwidth(sample_width)
+wav.setframerate(sampling_rate)
 
 for i in y:
-    f.write(struct.pack('b', int(i)))
-f.close()
+    wav.writeframesraw(struct.pack('b', int(i)))
+wav.close()
+
+# wave.open('test.wav', mode='rb')
 
 # Use command:
 # play -t raw -r 44.1k -e signed -b 8 -c 1 test.wav
+
+
